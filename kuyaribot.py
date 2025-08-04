ce78b8c7f40ee2085d726750f57e1a9fe0e762cc
Kylapro <gummyb126328@gmail.com>
2025-08-04 17:20:41 -0600

    Update kuyaribot.py

--- kuyaribot.py
+++ kuyaribot.py
@@ -46,7 +46,7 @@ last_task_time = 0
 
 intents = discord.Intents.default()
 intents.message_content = True
-activity = discord.CustomActivity(name=(config["status_message"] or "github.com/jakobdylanc/llmcord")[:128])
+activity = discord.CustomActivity(name=(config["status_message"] or "github.com/Kylapro/Kuyari-Bot")[:128])
 discord_bot = commands.Bot(intents=intents, activity=activity, command_prefix=None)
 
 httpx_client = httpx.AsyncClient(timeout=120.0)
