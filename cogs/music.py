2e2ee9a8d14cd0d4c115e35660492b9cc919f970
Kylapro <gummyb126328@gmail.com>
2025-08-04 21:23:48 -0600

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -13,7 +13,7 @@ YTDL_OPTS = {
     "quiet": True,
     "no_warnings": True,
     "default_search": "auto",
-    "noplaylist": True,
+    "noplaylist": False,
 }
 FFMPEG_OPTS = {
     "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
